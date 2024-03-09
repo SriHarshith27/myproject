dd4518a41c82d2a7232c928db6c0a54176e66847
Sri Harshith Nadella <sriharshith515@gmail.com>
2024-03-09 21:55:45 +0530

    yeeha

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <footer className="py-12 md:px-16 px-3 bg-slate-100 dark:bg-gray-900 flex md:flex-row flex-col md:justify-between justify-center items-center gap-4">
       <span className="md:text-xl  text-lg font-[600] text-gray-700 dark:text-white">
-        Copyright {year} | All Right Reserved
+        Copyright {year} | Harshith27 All Right Reserved
       </span>
       <div className="flex gap-5 items-center">
         <a
@@ -17,19 +17,19 @@ export default function Footer() {
           <BsFacebook />
         </a>
         <a
-          href="#"
+          href="https://www.instagram.com/_harshith_27/"
           className="md:text-3xl text-xl text-gray-900 dark:text-slate-50 hover:text-gray-500 dark:hover:text-slate-300"
         >
           <BsInstagram />
         </a>
         <a
-          href="#"
+          href="https://www.linkedin.com/in/thulasi-ram-43b052261/"
           className="md:text-3xl text-xl text-gray-900 dark:text-slate-50 hover:text-gray-500 dark:hover:text-slate-300"
         >
           <BsLinkedin />
         </a>
         <a
-          href="#"
+          href="https://twitter.com/Harshith273"
           className="md:text-3xl text-xl text-gray-900 dark:text-slate-50 hover:text-gray-500 dark:hover:text-slate-300"
         >
           <BsTwitter />
